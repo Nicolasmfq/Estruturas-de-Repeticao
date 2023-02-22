df677508d8969594be399fb01c143cf883d2a027
Nicolasmfq <nicolas.engsoftware@gmail.com>
2023-02-22 10:03:40 -0300

    Exercicio 1 Array

--- Estrutura-de-Repeticao/src/Array/OrdemInversa.java
+++ Estrutura-de-Repeticao/src/Array/OrdemInversa.java
@@ -6,7 +6,19 @@ package Array;
 public class OrdemInversa {
     public static void main(String[] args) {
         
+        int vetor[] = {50, 5, 16, -8, 19, 23};
+        int count = 0;
+
+        System.out.println("Vetor: ");
+        while(count < (vetor.length)){    
+            System.out.println(vetor[count] + " ");
+            count++;
+        }
         
+        System.out.println("Ordem Inversa: ");
+        for (int i = (vetor.length - 1); i >= 0; i--){
+            System.out.println(vetor[i] + " ");
+        }
 
     }
 }
